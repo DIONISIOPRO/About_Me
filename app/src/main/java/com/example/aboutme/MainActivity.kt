96e2632cd909b74f117921ee4000b081dbf06c8e
DIONISIOPRO <dionisiopaulonamuetho@gmail.com>
2021-03-22 17:44:01 +0200

    The last commit, an appis done

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val MyName : MyName = MyName("Dionisio")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = MyName
+        binding.button.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+            binding.apply {
+                invalidateAll()
+                myName?.nickName = nicknameEdit.text.toString()
+                nicknameEdit.visibility = View.GONE
+                nicknameText.visibility = View.VISIBLE
+                button.visibility = View.GONE
+            }
+           view.visibility = View.GONE
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
